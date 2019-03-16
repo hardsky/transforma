18e21687cc5a38cad44f7cde2724e00872239db8
Nikolay Mihailov <hardsky@yandex.ru>
2019-03-16 10:56:36 +0300

    clean up tests

--- internal/transforma/transforma_test.go
+++ internal/transforma/transforma_test.go
@@ -1,10 +1,53 @@
 package transforma
 
-import "testing"
+import (
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+)
 
 func TestGenerateAnyNames(t *testing.T) {
-	err := Generate("./test-data/any-names")
-	if err != nil {
-		t.Error(err)
+	testDataDir := "./test-data"
+	gotFile := "mapper_gen.go"
+	wantFile := "mapper-want.txt"
+
+	cases := []string{
+		"any-names",
+		"one-file",
+		"same-structs",
+	}
+
+	for _, c := range cases {
+		pkgPath := filepath.Join(testDataDir, c)
+		t.Log("package:", pkgPath)
+
+		err := Generate("./" + pkgPath)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+
+		gotFileName := filepath.Join(pkgPath, gotFile)
+		t.Log("got file:", gotFileName)
+
+		got, err := ioutil.ReadFile(gotFileName)
+		if err != nil {
+			t.Error(err)
+			continue
+
+		}
+
+		wantFileName := filepath.Join(pkgPath, wantFile)
+		t.Log("want file:", wantFileName)
+
+		want, err := ioutil.ReadFile(wantFileName)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+
+		if string(want) != string(got) {
+			t.Errorf("error with %q test", c)
+		}
 	}
 }
